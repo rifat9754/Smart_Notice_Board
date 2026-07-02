3d3b4608cf317d80b458a77cc139a183528203ae
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2026-07-02 15:56:08 +0600

    Fix some error

--- app/Http/Controllers/Api/NoticeApiController.php
+++ app/Http/Controllers/Api/NoticeApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Notice;
 use App\Models\NoticeView;
+use Illuminate\Http\Request;
 
 class NoticeApiController extends Controller
 {
