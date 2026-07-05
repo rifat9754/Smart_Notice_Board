6ef8cd6082891e268946975ee18b8c5970b3119b
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2026-07-06 03:24:25 +0600

    Fix storage permission

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,7 @@ use App\Models\Event;
 use App\Models\AuditLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class EventController extends Controller
 {
