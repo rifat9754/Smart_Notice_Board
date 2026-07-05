7d8ddec19a95a59fb24a951d2e141d46f3e9272e
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2026-07-06 04:06:43 +0600

    Fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-php artisan migrate --force 
+php artisan migrate --force --seed
 php artisan storage:link || true
 
 php-fpm -D
