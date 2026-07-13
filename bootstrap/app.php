aa61436ee3c4a0792a21193a12922f12e1c3c41c
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2026-07-14 03:46:46 +0600

    Fix signed URL validation behind Render proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -17,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         'role' => \App\Http\Middleware\RoleMiddleware::class,
         'preventback' => \App\Http\Middleware\PreventBackHistory::class,
     ]);
+
+    $middleware->trustProxies(at: '*');
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
